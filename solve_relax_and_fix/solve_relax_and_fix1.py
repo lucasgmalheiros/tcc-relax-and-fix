da7b811f96c8328e26323549869cfa2f577c5bc2
Lucas Malheiros <lgdm1303@gmail.com>
2024-10-27 20:43:48 -0300

    add test relax-and-fix

---
+++ solve_relax_and_fix/solve_relax_and_fix1.py
@@ -0,0 +1,20 @@
+from functions_model_sy_rf import main_solve_rf
+import numpy as np
+from gurobipy import GRB
+
+# Parâmetros
+    # Conjunto de instâncias (1 a 4)
+CONJUNTO = 1
+    # Time limit
+TIME_LIMIT = 1800
+
+# Arquivos
+    # Arquivo de salvamento
+results_csv = f'resultados{CONJUNTO}.csv'
+    # Pasta com instâncias a serem resolvidas
+folder_instancias = f'conjunto{CONJUNTO}/'
+
+w = 3  # Window
+y = 1  # Overlap
+
+main_solve_rf(folder_instancias, results_csv, TIME_LIMIT, window_size=w, overlap_size=y)

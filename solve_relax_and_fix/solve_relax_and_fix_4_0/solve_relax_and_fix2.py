c8e723720ee232b6b92d13fc0cf5b8cf9509ca13
Lucas Malheiros <lgdm1303@gmail.com>
2024-11-26 18:48:35 -0300

    Resultados RF 4_0

---
+++ solve_relax_and_fix/solve_relax_and_fix_4_0/solve_relax_and_fix2.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append(r'../')
+from functions_model_sy_rf import main_solve_rf
+
+# Parâmetros
+    # Conjunto de instâncias (1 a 4)
+CONJUNTO = 2
+    # Time limit
+TIME_LIMIT = 1800
+
+# Arquivos
+    # Arquivo de salvamento
+results_csv = f'resultados{CONJUNTO}.csv'
+    # Pasta com instâncias a serem resolvidas
+folder_instancias = f'../../instancias/conjunto{CONJUNTO}/'
+
+w = 4  # Window
+y = 0  # Overlap
+
+main_solve_rf(folder_instancias, results_csv, TIME_LIMIT, window_size=w, overlap_size=y)
